5b1ddff6fc68b7a17a75c7723402054008ad4dbf
Osman Ali <okali@uci.edu>
2018-10-17 10:38:31 -0700

    Change a elements without href to divs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -34,9 +34,9 @@ class Header extends React.Component {
                 <Link to="/"><h1>Cookout</h1></Link>
                 <div className="header__link-container">
                     <Link to="/host">Host</Link>
-                    {!isLoggedIn && <a onClick={this.handleLoginClick}>Login</a>}
-                    {isLoggedIn && <a>Bookings</a>}
-                    {isLoggedIn && <a>Saved</a>}
+                    {!isLoggedIn && <div onClick={this.handleLoginClick}>Login</div>}
+                    {isLoggedIn && <div>Bookings</div>}
+                    {isLoggedIn && <div>Saved</div>}
                     {isLoggedIn && <i onClick={this.handleLogoutClick} className="fas fa-user-circle profile-icon"></i>}
                 </div>
                 <LoginModal isOpen={this.state.modalShouldOpen} closeModal={this.closeModal} />
