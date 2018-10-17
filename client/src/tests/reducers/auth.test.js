f1c6c2d47be7213af9325935c8df57ebaa33fe27
Osman Ali <okali@uci.edu>
2018-10-17 09:18:39 -0700

    Add some tests and travisCI

---
+++ client/src/tests/reducers/auth.test.js
@@ -0,0 +1,33 @@
+import authReducer from '../../reducers/auth';
+
+const defaultState = {
+    token: '',
+    error: ''
+};
+
+describe('Auth reducer', () => {
+    it('correctly sets default state', () => {
+        const state = authReducer(undefined, { type: '@@INIT' });
+        expect(state).toEqual(defaultState); 
+    });
+
+    it('should set the token and remove any error', () => {
+        const token = 'abc123'
+        const state = authReducer(defaultState, {
+            type: 'SET_AUTH_TOKEN',
+            token
+        });
+        expect(state).toEqual({
+            token,
+            error: ''
+        });
+    });
+
+    it('should remove remove the token', () => {
+        const state = authReducer(defaultState, { type: 'REMOVE_AUTH_TOKEN' });
+        expect(state).toEqual({
+            token: '',
+            error: ''
+        });
+    });
+});
